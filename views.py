b83b131228b8650d636075b8f770cf06a925685d
z0rdd <jakub.usielski@gmail.com>
2017-07-20 13:39:08 +0200

    dfs

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, flash, url_for
 from webapp import app
-from models import db, stats
+
+# from models import db, stats
 
 
 # srf = CSRFProtect(app)
