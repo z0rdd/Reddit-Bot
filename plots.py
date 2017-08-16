4d9b1512c2f7331e95fdb9e0cfabc6f41fa22e58
z0rdd <jakub.usielski@gmail.com>
2017-08-16 13:10:19 +0200

    adding labels to graphs

--- plots.py
+++ plots.py
@@ -1,6 +1,6 @@
 from bokeh.plotting import figure, show
 from bokeh.embed import components
-from bokeh.models import DatetimeTickFormatter, HoverTool, Range1d, Legend
+from bokeh.models import DatetimeTickFormatter, HoverTool, Range1d, Legend, LabelSet, ColumnDataSource
 from datetime import datetime, timedelta
 
 ti_start = datetime(year=2017, month=8, day=2)
@@ -8,7 +8,7 @@ ti_end = datetime(year=2017, month=8, day=12)
 
 
 def main_plot(query):
-    # hover = HoverTool(tooltips=[("Date", "$x"),("Value", "$y")], formatters={"Date": "datetime"}, mode='vline')
+    # hover = HoverTool(tooltips=[("Date", "$x"),("Value", "$y")], formatters={"Date": "datetime"}, mode='mouse')
     c_tools = ['pan', 'wheel_zoom', 'save', 'reset']
     c = figure(x_axis_type="datetime", title="OSfrog mentions over time", tools=c_tools, logo=None, height=300,
                active_scroll='wheel_zoom')
@@ -23,22 +23,20 @@ def main_plot(query):
     c.ygrid.grid_line_alpha=0.2
     c.toolbar_location = None
     c.xaxis.formatter = DatetimeTickFormatter(days=['%y-%m-%d'])
-    # c.line(x=[item[0] for item in query],
-    #        y=[item[1] for item in query],
-    #        line_width=2,
-    #        color=(51, 122, 183),
-    #        alpha=1)
+
     max_range = []
     for item in query:
         max_range.append(item[1])
 
-    g1 = c.circle(x=[item[0] for item in query],
-             y=[item[1] for item in query],
-             size=10,
-             color=(51, 122, 183),
-             fill_color=(50, 150, 183),
-             alpha=1,
-             line_width=2)
+    src = ColumnDataSource(data=dict(x=[item[0] for item in query], y=[item[1] for item in query]))
+    g1 = c.circle(x='x',
+                  y='y',
+                  size=10,
+                  color=(51, 122, 183),
+                  fill_color=(50, 150, 183),
+                  alpha=1,
+                  line_width=2,
+                  source=src)
 
     g2 = c.quad(left=[ti_start],
                 right=[ti_end],
@@ -53,13 +51,17 @@ def main_plot(query):
                     orientation="horizontal")
 
     c.y_range = Range1d(min(max_range) - 20, max(max_range) + 20)
+
+    labels = LabelSet(x='x', y='y', text='y', source=src, text_font_size='11px', x_offset=-2, y_offset=4)
+
+    c.add_layout(labels)
     c.add_layout(legend, 'below')
 
     return components(c)
 
 
 def person_plot(query, name, count):
-    # hover = HoverTool(tooltips=[("Date", "$x"), ("Value", "$y")], formatters={"Date": "datetime"}, mode='vline')
+    # hover = HoverTool(tooltips=[("Date", "$x"), ("Value", "$y")], formatters={"Date": "datetime"}, mode='mouse')
     c_tools = ['pan', 'wheel_zoom', 'save', 'reset']
     title_str = "OSfrog mentions over time by " + name
     c = figure(x_axis_type="datetime", title=title_str, tools=c_tools, logo=None, height=300,
@@ -90,11 +92,15 @@ def person_plot(query, name, count):
     #        line_width=2,
     #        color=(51, 122, 183),
     #        alpha=1)
-    g1 = c.circle(x=[item[0] for item in query],
-                  y=[item[1] for item in query],
-                  size=10, color=(51, 122, 183),
+    src = ColumnDataSource(data=dict(x=[item[0] for item in query], y=[item[1] for item in query]))
+    g1 = c.circle(x='x',
+                  y='y',
+                  size=10,
+                  color=(51, 122, 183),
                   fill_color=(50, 150, 183),
-                  alpha=1, line_width=2)
+                  alpha=1,
+                  line_width=2,
+                  source=src)
 
     g2 = c.quad(left=[ti_start],
                 right=[ti_end],
@@ -108,6 +114,10 @@ def person_plot(query, name, count):
                     background_fill_alpha=0.0,
                     border_line_alpha=0.0,
                     orientation="horizontal")
+
+    labels = LabelSet(x='x', y='y', text='y', source=src, text_font_size='11px', x_offset=-2, y_offset=4)
+
+    c.add_layout(labels)
     c.add_layout(legend, 'below')
 
     return components(c)
